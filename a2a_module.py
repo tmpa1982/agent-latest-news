86ca4e7afcbf2b5b359266e8d0147ed3bbfe26af
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-10-23 08:14:31 +0700

    Add request handler & agent executor

--- a2a_module.py
+++ a2a_module.py
@@ -1,8 +1,12 @@
 from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
 from a2a.types import (
     AgentCapabilities,
     AgentCard,
 )
+
+from agent_executor import LatestNewsAgentExecutor
 
 class A2AModule:
     def __init__(self, host: str, port: int):
@@ -16,9 +20,13 @@ class A2AModule:
             capabilities=AgentCapabilities(),
             skills=[],
         )
+        request_handler = DefaultRequestHandler(
+            agent_executor=LatestNewsAgentExecutor(),
+            task_store=InMemoryTaskStore(),
+        )
         self.app = A2AStarletteApplication(
             agent_card=agent_card,
-            http_handler=None,
+            http_handler=request_handler,
         )
 
     def get_starlette(self):
---
+++ agent_executor.py
@@ -0,0 +1,18 @@
+from a2a.server.agent_execution import AgentExecutor, RequestContext
+from a2a.server.events import EventQueue
+from a2a.utils import new_agent_text_message
+
+class LatestNewsAgent:
+    async def invoke(self) -> str:
+        return "Typhoon Fengshen is making landfall the eastern coast of Vietnam, expect heavy rain and floods."
+
+class LatestNewsAgentExecutor(AgentExecutor):
+    def __init__(self):
+        self.agent = LatestNewsAgent()
+
+    async def execute(self, context: RequestContext, event_queue: EventQueue) -> None:
+        result = await self.agent.invoke()
+        await event_queue.enqueue_event(new_agent_text_message(result))
+
+    async def cancel(self, context: RequestContext, event_queue: EventQueue) -> None:
+        raise Exception('cancel not supported')
